c26f9fee3dc18e36e00520f6f1a6c748259d4e8d
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-07-02 16:58:07 +0530

    add-emp function implement and test for record insertion

--- src/app/page/manage-employee/manage-employee.component.ts
+++ src/app/page/manage-employee/manage-employee.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-employee',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HttpClientModule,CommonModule],
   templateUrl: './manage-employee.component.html',
   styleUrl: './manage-employee.component.css'
 })
 export class ManageEmployeeComponent {
 
+  public employeeObject={
+   firstName:"",
+   lastName:"",
+   email:"",
+   departmentId:"",
+   roleId:""
+  }
+  
+  constructor(private http:HttpClient){}
+
+  addEmployee(){
+    this.http.post("http://localhost:8080/emp-controller/add-employee",this.employeeObject).subscribe(
+      (data)=>{
+        console.log(data);
+      }
+    )
+  }
+  
 }
