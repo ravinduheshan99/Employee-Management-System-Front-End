1bd1262ea04d6b4336f9d7c25650b1b69d7824e7
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-07-07 02:39:30 +0530

    add a nav bar and create routing between add-emp and view-emp forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ManageEmployeeComponent } from './page/manage-employee/manage-employee.component';
+import { ViewAllEmployeeComponent } from './page/view-all-employee/view-all-employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"add-employee",
+    component:ManageEmployeeComponent
+  },
+  {
+    path:"view-employee",
+    component:ViewAllEmployeeComponent
+  }
+];
